a1e6547df7367a682850ec4becb89292ed17fd12
Gabs <gabrielrugil@gmail.com>
2024-04-05 11:56:51 -0300

    added teh residual connections, corrected the FF dimensions, reached 2.09 val loss

--- bigram_v2.py
+++ bigram_v2.py
@@ -97,16 +97,20 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd_dim, n_embd_dim)
 
     def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
     
 class FeedForward(nn.Module):
     def __init__(self, n_embd_dim):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd_dim, n_embd_dim),
+            nn.Linear(n_embd_dim, 4 * n_embd_dim),
             nn.ReLU(),
+            nn.Linear(4 * n_embd_dim, n_embd_dim), # projection layer
         )
     
     def forward(self, x):
@@ -121,8 +125,8 @@ class Block(nn.Module):
         self.ffwd = FeedForward(n_embd_dim) # computation
 
     def forward(self, x):
-        x = self.sa(x)
-        x - self.ffwd(x)
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
         return x
 class BiGramLM(nn.Module):
     def __init__(self):
